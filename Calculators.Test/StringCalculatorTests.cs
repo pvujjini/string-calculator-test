109e283821b2f72f8dc014256fd3ab411a072bd8
dbrunger <david.brunger@alpha9software.com>
2016-03-09 20:57:41 +0000

    Add any number of integers

--- Calculators.Test/StringCalculatorTests.cs
+++ Calculators.Test/StringCalculatorTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
 
 namespace Calculators.Test
 {
@@ -23,5 +24,36 @@ namespace Calculators.Test
             Assert.AreEqual(755, calc.Add("537,218"));
         }
 
+        /// <summary>
+        /// Tests adding an arbitrary number of integers
+        /// </summary>
+        /// <remarks>
+        /// I tried testing a much longer string than in the tests as detailed in the method comments.
+        /// As I was conscious of time I had to stick with what I had and leave the longer tests. Given
+        /// enough time a decent pratical limit for testing could be established 
+        /// </remarks>
+        [TestCase]
+        public void AddMoreThanTwoNumbers()
+        {
+            var calc = new StringCalculator();
+            Assert.AreEqual(0, calc.Add("0,0,0"));
+            Assert.AreEqual(0, calc.Add("0,0,0,0,0,0,0"));
+            Assert.AreEqual(0, calc.Add(string.Join(",", Enumerable.Range(1, 100000).Select(i => "0"))));
+            Assert.AreEqual(100000, calc.Add(string.Join(",", Enumerable.Range(1, 100000).Select(i => "1"))));
+
+            // The nth triangular number is given by: ((n * n) + n) / 2
+            Assert.AreEqual(((1000 * 1000) + 1000) / 2,
+                calc.Add(string.Join(",", Enumerable.Range(1, 1000).Select(i => i.ToString()))));
+
+            // A string this long causes an out of memory exception before the tested method is even called so a 100000 number string
+            // will have to do without spending a long time deciding what the maximum length can be
+            //
+            // var builder = new StringBuilder("1");
+            // while (builder.Length < int.MaxValue) // 2,147,483,647
+            // {
+            //     builder.Append(",1");
+            // } 
+            // Assert.AreEqual((int.MaxValue / 2) + 1, calc.Add(builder.ToString()));
+        }
     }
 }
--- Calculators/StringCalculator.cs
+++ Calculators/StringCalculator.cs
@@ -8,6 +8,17 @@ namespace Calculators
     /// </summary>
     public class StringCalculator
     {
+        /// <summary>
+        /// Adds a integers contained in a delimited string
+        /// </summary>
+        /// <param name="numbers">The delimited string containing the integers</param>
+        /// <returns>The sum of the integers</returns>
+        /// <remarks>
+        /// <para>
+        /// The string can contain zero to any number of integers, but they must be
+        /// delimited by commas with no spaces. E.g. "0,1,2"
+        /// </para>
+        /// </remarks>
         public int Add(string numbers)
         {
             return numbers.Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).Sum();
